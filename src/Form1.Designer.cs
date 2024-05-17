05b7604a9917ca5ff92f4259297ba4e5e37a94e2
Francesco Michael Kusuma <francescokusuma15@gmail.com>
2024-05-17 23:58:45 +0700

    form, upload image

---
+++ src/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace src
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.uploadButton = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBoxUploadedImage = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxUploadedImage)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // uploadButton
+            // 
+            this.uploadButton.ForeColor = System.Drawing.Color.Black;
+            this.uploadButton.Location = new System.Drawing.Point(142, 904);
+            this.uploadButton.Name = "uploadButton";
+            this.uploadButton.Size = new System.Drawing.Size(400, 122);
+            this.uploadButton.TabIndex = 0;
+            this.uploadButton.Text = "UPLOAD";
+            this.uploadButton.UseVisualStyleBackColor = true;
+            this.uploadButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(813, 199);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(400, 650);
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            // 
+            // button2
+            // 
+            this.button2.ForeColor = System.Drawing.Color.Black;
+            this.button2.Location = new System.Drawing.Point(813, 904);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(400, 122);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "SEARCH";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(674, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(705, 82);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Tugas Besar 3 Stima";
+            this.label1.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // pictureBoxUploadedImage
+            // 
+            this.pictureBoxUploadedImage.Location = new System.Drawing.Point(142, 199);
+            this.pictureBoxUploadedImage.Name = "pictureBoxUploadedImage";
+            this.pictureBoxUploadedImage.Size = new System.Drawing.Size(400, 650);
+            this.pictureBoxUploadedImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxUploadedImage.TabIndex = 6;
+            this.pictureBoxUploadedImage.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(1449, 199);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(400, 650);
+            this.pictureBox3.TabIndex = 7;
+            this.pictureBox3.TabStop = false;
+            // 
+            // button3
+            // 
+            this.button3.ForeColor = System.Drawing.Color.Black;
+            this.button3.Location = new System.Drawing.Point(1449, 904);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(400, 122);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "SEARCH";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Aqua;
+            this.ClientSize = new System.Drawing.Size(1978, 1144);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBoxUploadedImage);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.uploadButton);
+            this.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxUploadedImage)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button uploadButton;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBoxUploadedImage;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ src/Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace src
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // upload button
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if(openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                pictureBoxUploadedImage.Image = new Bitmap(openFileDialog.FileName);
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (pictureBoxUploadedImage.Image != null)
+            {
+                Bitmap bitmap = new Bitmap(pictureBoxUploadedImage.Image);
+                string binaryString = BmpToBinaryString(bitmap);
+                string asciiString = BinaryToAscii(binaryString);
+                Console.WriteLine(asciiString);
+            }
+            else
+            {
+                MessageBox.Show("Please upload an image first.");
+            }
+        }
+        private string BmpToBinaryString(Bitmap bmp)
+        {
+            Bitmap grayscaleBmp = new Bitmap(bmp.Width, bmp.Height);
+            for (int y = 0; y < bmp.Height; y++)
+            {
+                for (int x = 0; x < bmp.Width; x++)
+                {
+                    Color pixelColor = bmp.GetPixel(x, y);
+                    int grayValue = (int)(pixelColor.R * 0.299 + pixelColor.G * 0.587 + pixelColor.B * 0.114);
+                    grayscaleBmp.SetPixel(x, y, Color.FromArgb(grayValue, grayValue, grayValue));
+                }
+            }
+
+            int threshold = 128;
+            StringBuilder binaryString = new StringBuilder();
+
+            for (int y = 0; y < grayscaleBmp.Height; y++)
+            {
+                for (int x = 0; x < grayscaleBmp.Width; x++)
+                {
+                    Color grayPixelColor = grayscaleBmp.GetPixel(x, y);
+                    int grayValue = grayPixelColor.R;
+                    binaryString.Append(grayValue < threshold ? '1' : '0');
+                }
+            }
+
+            return binaryString.ToString();
+        }
+
+        private string BinaryToAscii(string binaryString)
+        {
+            StringBuilder asciiString = new StringBuilder();
+
+            for (int i = 0; i < binaryString.Length; i += 8)
+            {
+                if (i + 8 <= binaryString.Length)
+                {
+                    string byteString = binaryString.Substring(i, 8);
+                    int decimalValue = Convert.ToInt32(byteString, 2);
+                    asciiString.Append((char)decimalValue);
+                }
+            }
+
+            return asciiString.ToString();
+        }
+
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
